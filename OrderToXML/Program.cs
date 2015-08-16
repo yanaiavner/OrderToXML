8e37dd2a769fb667b99ef3f9f41b61ecc19943b6
yanaiavner <yanaiavner@gmail.com>
2015-08-16 10:38:15 -0500

    Rev2

--- OrderToXML/Program.cs
+++ OrderToXML/Program.cs
@@ -17,27 +17,52 @@ namespace OrderToXML
             using (var db = new ProductionDBEntities())
             {
                 var fileName = "Order.xml";
-                var orderLimit = 3;
-                var cts = new CancellationTokenSource();
-
-                if (File.Exists(fileName)) File.Delete(fileName);
-                //StreamWriter toFile = new StreamWriter(fileName);
                 StreamWriter toFile = null;
+                if (File.Exists(fileName)) File.Delete(fileName);
+                toFile = new StreamWriter(fileName);
 
-                WriteXMLLine(GetXMLLine(0, "Root"), toFile);
-                var result = SetOrdersXMLAsync(db, orderLimit, toFile);
-                result.Wait();
+                try
+                {
+                    
+                    var orderLimit = 0;
+                    var cts = new CancellationTokenSource();
 
-                WriteXMLLine(GetXMLLine(0, "Root", true), toFile);
+                    
+                    
 
 
-                if (toFile == null) Console.ReadKey();
+                    WriteXMLLine(GetXMLLine(0, "Root"), toFile);
+                    var result = SetOrdersXMLAsync(db, orderLimit, toFile, cts.Token);
 
-                if (toFile != null) toFile.Close();
+                    while (!result.IsCompleted)
+                    {
+                        if (Console.KeyAvailable)
+                        {
+                            Console.ReadKey();
+                            if (!cts.IsCancellationRequested)
+                            {
+                                cts.Cancel();
+                            }
+                        }
+                    }
+
+                    WriteXMLLine(GetXMLLine(0, "Root", true), toFile);
+                    
+                    
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+                finally
+                {
+                    if (toFile == null) Console.ReadKey();
+                    if (toFile != null) toFile.Close();
+                }
             }
         }
 
-        private static async Task SetOrdersXMLAsync(ProductionDBEntities db, int orderLimit, StreamWriter toFile)
+        private static async Task SetOrdersXMLAsync(ProductionDBEntities db, int orderLimit, StreamWriter toFile, CancellationToken token)
         {
             var query = from o in db.Orders
                         orderby o.Name
@@ -47,9 +72,19 @@ namespace OrderToXML
 
             foreach (var order in query)
             {
+                if (token.IsCancellationRequested)
+                {
+                    break;
+                }
                 orderCounter++;
                 if (orderLimit > 0 && orderCounter > orderLimit) break;
                 await Task.Run(() => SetOrderTagXML(db, toFile, order));
+
+                if(toFile != null)
+                {
+                    Console.Write(orderCounter);
+                    Console.CursorLeft = 0;
+                }
             }
         }
 
@@ -67,16 +102,38 @@ namespace OrderToXML
 
             foreach (var orderPackage in orderPakcages)
             {
-                var pakcage = from pk in db.Packages where pk.PackageID == orderPackage.PackageID select pk;
+                var pakcage = (from pk in db.Packages where pk.PackageID == orderPackage.PackageID select pk).FirstOrDefault();
+                if (pakcage == null) continue;
 
                 WriteXMLLine(GetXMLLine(2, "Packages", false, orderPackage.PackageID.ToString(), true, "ID"), toFile);
-                if (pakcage.Count() > 0)
+                WriteXMLLine(GetXMLLine(3, "PackageID", false, pakcage.PackageID.ToString()), toFile);
+                WriteXMLLine(GetXMLLine(3, "Name", false, pakcage.Name), toFile);
+                WriteXMLLine(GetXMLLine(3, "PackageStateID", false, pakcage.PackageStateID.ToString()), toFile);
+                WriteXMLLine(GetXMLLine(3, "PackageTypeID", false, pakcage.PackageTypeID.ToString()), toFile);
+                WriteXMLLine(GetXMLLine(3, "PDF", false, pakcage.PDF), toFile);
+
+                var packageParts = from pp in db.PackageParts where pp.PackageID == pakcage.PackageID select pp;
+
+                foreach (var pPart in packageParts)
                 {
-                    WriteXMLLine(GetXMLLine(3, "Name", false, pakcage.FirstOrDefault().Name), toFile);
-                    WriteXMLLine(GetXMLLine(3, "PackageStateID", false, pakcage.FirstOrDefault().PackageStateID.ToString()), toFile);
-                    WriteXMLLine(GetXMLLine(3, "PackageTypeID", false, pakcage.FirstOrDefault().PackageTypeID.ToString()), toFile);
-                    WriteXMLLine(GetXMLLine(3, "PDF", false, pakcage.FirstOrDefault().PDF), toFile);
+                    var part = (from p in db.Parts where p.PartID == pPart.PartID select p).FirstOrDefault();
+                    if (part == null) continue;
+
+                    WriteXMLLine(GetXMLLine(3, "Part", false, part.PartID.ToString(), true, "ID"), toFile);
+                    WriteXMLLine(GetXMLLine(4, "PartID", false, part.PartID.ToString()), toFile);
+                    WriteXMLLine(GetXMLLine(4, "Number", false, part.PartNo), toFile);
+                    WriteXMLLine(GetXMLLine(4, "QuantityOnHand", false, part.QuantityOnHand.ToString()), toFile);
+                    WriteXMLLine(GetXMLLine(4, "Description", false, part.Description.Trim()), toFile);
+                    WriteXMLLine(GetXMLLine(4, "ProceesTypeID", false, part.ProceesTypeID.ToString()), toFile);
+
+                    WriteXMLLine(GetXMLLine(4, "Vendor", false, part.VendorID.ToString(), true, "ID"), toFile);
+                    WriteXMLLine(GetXMLLine(5, "VendorID", false, part.Vendor.VendorID.ToString()), toFile);
+                    WriteXMLLine(GetXMLLine(5, "Name", false, part.Vendor.VendorName), toFile);
+                    WriteXMLLine(GetXMLLine(4, "Vendor", true), toFile);
+
+                    WriteXMLLine(GetXMLLine(3, "Part", true), toFile);
                 }
+
                 WriteXMLLine(GetXMLLine(2, "Packages", true), toFile);
             }
 
